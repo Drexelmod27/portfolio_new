26679d508ada3cb533f53f62246fec482559a0b1
Mark Deleon <delavierdev@gmail.com>
2019-11-05 19:30:20 -0500

    work

--- wp-content/themes/fleur/framework/modules/portfolio/templates/single/parts/categories.php
+++ wp-content/themes/fleur/framework/modules/portfolio/templates/single/parts/categories.php
@@ -1,15 +1,15 @@
-<?php if(fleur_mikado_options()->getOptionValue('portfolio_single_hide_categories') !== 'yes') : ?>
+<?php if (fleur_mikado_options()->getOptionValue('portfolio_single_hide_categories') !== 'yes') : ?>
 
 	<?php
-	$categories   = wp_get_post_terms(get_the_ID(), 'portfolio-category');
-	$categy_names = array();
+		$categories   = wp_get_post_terms(get_the_ID(), 'portfolio-category');
+		$categy_names = array();
 
-	if(is_array($categories) && count($categories)) :
-		foreach($categories as $category) {
-			$categy_names[] = $category->name;
-		}
+		if (is_array($categories) && count($categories)) :
+			foreach ($categories as $category) {
+				$categy_names[] = $category->name;
+			}
 
-		?>
+			?>
 		<div class="mkd-portfolio-item-categories">
 			<h5 class="mkd-portfolio-item-category"><?php echo esc_html(implode(', ', $categy_names)); ?></h5>
 		</div>
